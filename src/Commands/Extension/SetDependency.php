92a1eedf54a8a7dfbdc3a7fd0a8a53447e591611
Matthew Batchelder <borkweb@gmail.com>
2020-11-20 08:40:53 -0500

    Adding .env comments

--- src/Commands/Extension/SetDependency.php
+++ src/Commands/Extension/SetDependency.php
@@ -1,8 +1,6 @@
 <?php
 namespace TUT\Commands\Extension;
 
-use Dotenv;
-use Github;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output;
