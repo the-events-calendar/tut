7bec834740ab27c45804715572e483ac11940277
Matthew Batchelder <borkweb@gmail.com>
2021-05-23 14:38:26 -0400

    Add a command to get the zip filename
    This is a helper command to generate zip filenames.

---
+++ docs/commands/get-zip-filename.md
@@ -0,0 +1,26 @@
+# `tut get-zip-filename`
+
+**Gets a plugins expected filename for zipping.**
+
+Using the current state of the branch, this command generates the filename that would be used when zip packaging.
+
+## Usage
+
+```bash
+tut get-zip-filename
+```
+
+## Args
+
+```
+Usage:
+  get-zip-filename
+
+Options:
+      --final           Get the zip filename for production.
+  -h, --help            Display this help message
+
+Help:
+  Gets the expected zip filename for the plugin.
+
+```
---
+++ src/Commands/GetZipFilename.php
@@ -0,0 +1,80 @@
+<?php
+namespace TUT\Commands;
+
+use TUT\Command as Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class GetZipFilename extends Command {
+
+	/**
+	 * @var bool Should the command prompt for plugin selection?
+	 */
+	public $do_plugin_selection = true;
+
+	/**
+	 * @var string The branch in which the version is being prepared
+	 */
+	protected $branch;
+
+	protected function configure() {
+		parent::configure();
+
+		$this
+			->setName( 'get-zip-filename' )
+			->setDescription( 'Gets the expected zip filename for the plugin.' )
+			->setHelp( 'Gets the expected zip filename for the plugin.' )
+			->addOption( 'final', '', InputOption::VALUE_NONE, 'Get the zip filename for production.' );
+		;
+	}
+
+	protected function execute( InputInterface $input, OutputInterface $output ) {
+		$final = (bool) $input->getOption( 'final' );
+
+		foreach ( $this->selected_plugins as $plugin ) {
+			$plugin_dir = $this->get_plugin_dir( $plugin );
+
+			if ( ! file_exists( $plugin_dir ) ) {
+				$this->io->error( "The {$plugin->name} directory doesn't exist here!" );
+				return 1;
+			}
+
+			// cd into the plugin directory
+			chdir( $plugin_dir );
+
+			$zip_name = $plugin->name;
+
+			if ( file_exists( 'package.json' ) ) {
+				$package_json = json_decode( file_get_contents( 'package.json' ) );
+				$zip_name     = $package_json->_zipname;
+			}
+
+			if ( ! file_exists( $plugin->main ) ) {
+				$this->io->error( "Could not find {$plugin->main}" );
+				return 1;
+			}
+
+			$file = file_get_contents( $plugin->main );
+
+			preg_match( "/.*{$plugin->version}[^']*'([^']*)'.*/", $file, $matches );
+
+			$version   = $matches[1];
+			$hash      = trim( shell_exec( 'git rev-parse --short=8 HEAD' ) );
+			$timestamp = trim( shell_exec( 'git --no-pager show -s --format=%ct HEAD' ) );
+			$filename  = "{$zip_name}.{$version}-dev-{$timestamp}-${hash}.zip";
+
+			if ( $final ) {
+				$filename  = "{$zip_name}.{$version}.zip";
+			}
+
+			$output->writeln( $filename );
+
+			// go back up to the plugins directory
+			chdir( '../' );
+		}//end foreach
+
+		return 0;
+	}
+}
