9eeac9aeb668ef945b95fc2e7f7c3980721c3f12
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-07-13 12:15:26 -0400

    Fix spacing

--- src/Commands/SvnTag.php
+++ src/Commands/SvnTag.php
@@ -9,229 +9,228 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SvnTag extends Command {
-    protected const CMD_SUCCESS = 0;
+	protected const CMD_SUCCESS = 0;
 
-    protected const CMD_FAILURE = 1;
+	protected const CMD_FAILURE = 1;
 
-    protected const WP_ORG_URL =  'https://plugins.svn.wordpress.org/';
+	protected const WP_ORG_URL = 'https://plugins.svn.wordpress.org/';
 
-    protected function configure() {
-        $temp_dir = '/tmp/svn-tag/';
+	protected function configure() {
+		$temp_dir = '/tmp/svn-tag/';
 
-        $this
-            ->setName('svn:tag')
-            ->setDescription('Creates a new SVN tag and applies changes from a ZIP file.')
-            ->setHelp(  'This command allows you to create a new SVN tag from an existing tag and apply changes from a ZIP file.')
-            ->addArgument( 'plugin', InputArgument::REQUIRED, 'The URL of the SVN repository.' )
-            ->addArgument( 'source_tag', InputArgument::REQUIRED, 'The SVN tag to be copied.' )
-            ->addArgument( 'destination_tag', InputArgument::REQUIRED, 'The new SVN tag.' )
-            ->addOption( 'zip_url', 'z', InputOption::VALUE_OPTIONAL, 'The URL of the ZIP file with changes to apply.' )
-            ->addOption( 'temp_dir', 't', InputOption::VALUE_OPTIONAL, 'The temporary directory to use.', $temp_dir );
-    }
+		$this
+			->setName( 'svn:tag' )
+			->setDescription( 'Creates a new SVN tag and applies changes from a ZIP file.' )
+			->setHelp( 'This command allows you to create a new SVN tag from an existing tag and apply changes from a ZIP file.' )
+			->addArgument( 'plugin', InputArgument::REQUIRED, 'The URL of the SVN repository.' )
+			->addArgument( 'source_tag', InputArgument::REQUIRED, 'The SVN tag to be copied.' )
+			->addArgument( 'destination_tag', InputArgument::REQUIRED, 'The new SVN tag.' )
+			->addOption( 'zip_url', 'z', InputOption::VALUE_OPTIONAL, 'The URL of the ZIP file with changes to apply.' )
+			->addOption( 'temp_dir', 't', InputOption::VALUE_OPTIONAL, 'The temporary directory to use.', $temp_dir );
+	}
 
-    protected function execute( InputInterface $input, OutputInterface $output ) {
-        @ini_set( 'memory_limit', '2048M' );
+	protected function execute( InputInterface $input, OutputInterface $output ) {
+		@ini_set( 'memory_limit', '2048M' );
 
-        $id = date( 'Y-m-d-H-m-s' ) . '-' .  substr( uniqid(), 0, 12 );
-        $plugin = $input->getArgument( 'plugin' );
-        $source_tag = $input->getArgument( 'source_tag' );
-        $destination_tag = $input->getArgument( 'destination_tag' );
-        $zip_url = $input->getOption( 'zip_url' );
-        $temp_dir = $input->getOption( 'temp_dir' );
+		$id              = date( 'Y-m-d-H-m-s' ) . '-' . substr( uniqid(), 0, 12 );
+		$plugin          = $input->getArgument( 'plugin' );
+		$source_tag      = $input->getArgument( 'source_tag' );
+		$destination_tag = $input->getArgument( 'destination_tag' );
+		$zip_url         = $input->getOption( 'zip_url' );
+		$temp_dir        = $input->getOption( 'temp_dir' );
 
-        $temp_dir = rtrim( $temp_dir, '/' ) . "/tag-{$id}";
+		$temp_dir = rtrim( $temp_dir, '/' ) . "/tag-{$id}";
 
-        if ( empty( $local_repo ) ) {
-            $local_repo = "{$temp_dir}/repo";
-        }
-        $svn_url = self::WP_ORG_URL . $plugin;
+		if ( empty( $local_repo ) ) {
+			$local_repo = "{$temp_dir}/repo";
+		}
+		$svn_url = self::WP_ORG_URL . $plugin;
 
-        // Log the arguments and options
-        $output->writeln( 'Plugin: ' . $plugin );
-        $output->writeln( 'SVN URL: ' . $svn_url );
-        $output->writeln( 'Source Tag: ' . $source_tag );
-        $output->writeln( 'Destination Tag: ' . $destination_tag );
-        $output->writeln( 'ZIP URL: ' . $zip_url );
-        $output->writeln( 'Temporary Directory: ' . $temp_dir );
+		// Log the arguments and options
+		$output->writeln( 'Plugin: ' . $plugin );
+		$output->writeln( 'SVN URL: ' . $svn_url );
+		$output->writeln( 'Source Tag: ' . $source_tag );
+		$output->writeln( 'Destination Tag: ' . $destination_tag );
+		$output->writeln( 'ZIP URL: ' . $zip_url );
+		$output->writeln( 'Temporary Directory: ' . $temp_dir );
 
-        $plugin_dir = $temp_dir . '/plugin/';
+		$plugin_dir = $temp_dir . '/plugin/';
 
-        // Create temporary directory if it doesn't exist
-        if ( ! is_dir( $temp_dir ) ) {
-            $output->writeln( 'Temporary directory does not exist. Creating: ' . $temp_dir );
-            mkdir( $temp_dir, 0755, true );
-        }
+		// Create temporary directory if it doesn't exist
+		if ( ! is_dir( $temp_dir ) ) {
+			$output->writeln( 'Temporary directory does not exist. Creating: ' . $temp_dir );
+			mkdir( $temp_dir, 0755, true );
+		}
 
-        // Ensure the plugin extraction dir is empty before doing anything.
-        if ( file_exists( $plugin_dir ) ) {
-            shell_exec( "rm -rf {$plugin_dir}" );
-            mkdir( $plugin_dir, 0755, true );
-        }
+		// Ensure the plugin extraction dir is empty before doing anything.
+		if ( file_exists( $plugin_dir ) ) {
+			shell_exec( "rm -rf {$plugin_dir}" );
+			mkdir( $plugin_dir, 0755, true );
+		}
 
-        // Download and extract ZIP file to temporary directory
-        $zip_file = rtrim( $temp_dir, '/' ) . '/plugin.zip';
+		// Download and extract ZIP file to temporary directory
+		$zip_file = rtrim( $temp_dir, '/' ) . '/plugin.zip';
 
-        // just to make sure we can download the new file.
-        if ( file_exists( $zip_file ) && is_file( $zip_file ) ) {
-            unlink( $zip_file );
-        }
+		// just to make sure we can download the new file.
+		if ( file_exists( $zip_file ) && is_file( $zip_file ) ) {
+			unlink( $zip_file );
+		}
 
-        $output->writeln( 'Downloading ZIP file to: ' . $zip_file );
+		$output->writeln( 'Downloading ZIP file to: ' . $zip_file );
 
-        file_put_contents( $zip_file, file_get_contents( $zip_url ) );
-        $output->writeln( 'Extracting ZIP file to: ' . $temp_dir );
-        $zip = new \ZipArchive();
+		file_put_contents( $zip_file, file_get_contents( $zip_url ) );
+		$output->writeln( 'Extracting ZIP file to: ' . $temp_dir );
+		$zip = new \ZipArchive();
 
-        if ( $zip->open( $zip_file ) === TRUE ) {
-            $zip->extractTo( $temp_dir );
-            $zip->close();
+		if ( $zip->open( $zip_file ) === true ) {
+			$zip->extractTo( $temp_dir );
+			$zip->close();
 
-            shell_exec( "mv {$temp_dir}/{$plugin} $plugin_dir" );
-            if ( ! file_exists( $plugin_dir ) ) {
-                $output->writeln( "<error>ZIP was not extracted correctly, expected destination: {$plugin_dir}.</error>" );
-            }
-        } else {
-            $output->writeln( '<error>Failed to open ZIP file.</error>' );
-            return self::CMD_FAILURE;
-        }
+			shell_exec( "mv {$temp_dir}/{$plugin} $plugin_dir" );
+			if ( ! file_exists( $plugin_dir ) ) {
+				$output->writeln( "<error>ZIP was not extracted correctly, expected destination: {$plugin_dir}.</error>" );
+			}
+		} else {
+			$output->writeln( '<error>Failed to open ZIP file.</error>' );
+			return self::CMD_FAILURE;
+		}
 
-        $cd_to_repo_cmd = "cd {$local_repo} && ";
+		$cd_to_repo_cmd = "cd {$local_repo} && ";
 
-        $clone_cmd = "svn co {$svn_url} {$local_repo} --depth=immediates";
-        $output->writeln( "Executing SVN Command: \n" . $clone_cmd );
-        shell_exec( $clone_cmd );
+		$clone_cmd = "svn co {$svn_url} {$local_repo} --depth=immediates";
+		$output->writeln( "Executing SVN Command: \n" . $clone_cmd );
+		shell_exec( $clone_cmd );
 
-        $update_trunk_cmd = "svn update trunk/readme.txt";
-        $output->writeln( "Executing SVN Command: \n" . $update_trunk_cmd );
-        shell_exec( $cd_to_repo_cmd . $update_trunk_cmd );
+		$update_trunk_cmd = "svn update trunk/readme.txt";
+		$output->writeln( "Executing SVN Command: \n" . $update_trunk_cmd );
+		shell_exec( $cd_to_repo_cmd . $update_trunk_cmd );
 
+		$update_tag_trunk_cmd = "svn update tags/{$source_tag}";
+		$output->writeln( "Executing SVN Command: \n" . $update_tag_trunk_cmd );
+		shell_exec( $cd_to_repo_cmd . $update_tag_trunk_cmd );
 
-        $update_tag_trunk_cmd = "svn update tags/{$source_tag}";
-        $output->writeln( "Executing SVN Command: \n" . $update_tag_trunk_cmd );
-        shell_exec( $cd_to_repo_cmd . $update_tag_trunk_cmd );
+		// Copy SVN tag
+		$copy_cmd = "svn copy {$svn_url}/tags/{$source_tag} {$svn_url}/tags/{$destination_tag} -m 'Copying {$source_tag} into {$destination_tag}'";
+		$output->writeln( "Executing SVN Command:  \n" . $copy_cmd );
+		shell_exec( $copy_cmd );
 
-        // Copy SVN tag
-        $copy_cmd = "svn copy {$svn_url}/tags/{$source_tag} {$svn_url}/tags/{$destination_tag} -m 'Copying {$source_tag} into {$destination_tag}'";
-        $output->writeln( "Executing SVN Command:  \n" . $copy_cmd );
-        shell_exec( $copy_cmd );
+		// Update local repository
+		$update_cmd = "svn update tags/{$destination_tag}";
+		$output->writeln( "Executing SVN Command: \n" . $update_cmd );
+		shell_exec( $cd_to_repo_cmd . $update_cmd );
 
-        // Update local repository
-        $update_cmd = "svn update tags/{$destination_tag}";
-        $output->writeln( "Executing SVN Command: \n" . $update_cmd );
-        shell_exec( $cd_to_repo_cmd . $update_cmd );
+		// Compare tag folder and extracted folder
+		$source_tag_folder      = "{$local_repo}/tags/{$source_tag}";
+		$destination_tag_folder = "{$local_repo}/tags/{$destination_tag}";
 
-        // Compare tag folder and extracted folder
-        $source_tag_folder = "{$local_repo}/tags/{$source_tag}";
-        $destination_tag_folder = "{$local_repo}/tags/{$destination_tag}";
+		// Delete tag folder
+		$delete_cmd = "rm -rf {$destination_tag_folder}";
+		$output->writeln( "Removing locally the destination tag: \n" . $delete_cmd );
+		shell_exec( $delete_cmd );
 
-        // Delete tag folder
-        $delete_cmd = "rm -rf {$destination_tag_folder}";
-        $output->writeln( "Removing locally the destination tag: \n" . $delete_cmd );
-        shell_exec( $delete_cmd );
+		// Move extracted files to tag folder
+		$move_cmd = "mv {$plugin_dir} {$destination_tag_folder}";
+		$output->writeln( "Move the ZIP version of the destination tag to the correct folder: \n" . $move_cmd );
+		shell_exec( $move_cmd );
 
-        // Move extracted files to tag folder
-        $move_cmd = "mv {$plugin_dir} {$destination_tag_folder}";
-        $output->writeln( "Move the ZIP version of the destination tag to the correct folder: \n" . $move_cmd );
-        shell_exec( $move_cmd );
+		$scan = $this->compare_directories( $source_tag_folder, $destination_tag_folder );
 
-        $scan = $this->compare_directories( $source_tag_folder, $destination_tag_folder );
+		$output->writeln( "Comparing Directories:" );
+		$output->writeln( " - $source_tag_folder" );
+		$output->writeln( " - $destination_tag_folder" );
 
-        $output->writeln( "Comparing Directories:" );
-        $output->writeln( " - $source_tag_folder" );
-        $output->writeln( " - $destination_tag_folder" );
+		if ( null === $scan ) {
+			$output->writeln( '<error>Error while trying to compare folders.</error>' );
+			return self::CMD_FAILURE;
+		}
 
-        if ( null === $scan ) {
-            $output->writeln('<error>Error while trying to compare folders.</error>');
-            return self::CMD_FAILURE;
-        }
-
-        if ( ! empty( $scan['added'] ) ) {
+		if ( ! empty( $scan['added'] ) ) {
 			$output->writeln( '' );
 
-            $output->writeln( "Added files: \n - " . implode( "\n - ", $scan['added'] ) );
-
-			$output->writeln( '' );
-
-            $output->writeln( "Including all added files from destination Tag folder: " );
-            foreach ( $scan['added'] as $file ) {
-                $add_cmd = "svn add --parents tags/{$destination_tag}/{$file}";
-                $output->writeln( $add_cmd );
-                shell_exec( $cd_to_repo_cmd . $add_cmd );
-            }
-        }
-
-        if ( ! empty( $scan['deleted'] ) ) {
-			$output->writeln( '' );
-
-            $output->writeln( "Deleted files: \n - " . implode( "\n - ", $scan['deleted'] ) );
+			$output->writeln( "Added files: \n - " . implode( "\n - ", $scan['added'] ) );
 
 			$output->writeln( '' );
 
-            // Remove deleted files
-            $output->writeln( "Deleting all removed files from destination Tag folder:" );
-            foreach ( $scan['deleted'] as $file ) {
-                $remove_cmd = "svn rm tags/{$destination_tag}/{$file}";
-                $output->writeln( $remove_cmd );
-                shell_exec( $cd_to_repo_cmd . $remove_cmd );
-            }
-        }
+			$output->writeln( "Including all added files from destination Tag folder: " );
+			foreach ( $scan['added'] as $file ) {
+				$add_cmd = "svn add --parents tags/{$destination_tag}/{$file}";
+				$output->writeln( $add_cmd );
+				shell_exec( $cd_to_repo_cmd . $add_cmd );
+			}
+		}
+
+		if ( ! empty( $scan['deleted'] ) ) {
+			$output->writeln( '' );
+
+			$output->writeln( "Deleted files: \n - " . implode( "\n - ", $scan['deleted'] ) );
+
+			$output->writeln( '' );
+
+			// Remove deleted files
+			$output->writeln( "Deleting all removed files from destination Tag folder:" );
+			foreach ( $scan['deleted'] as $file ) {
+				$remove_cmd = "svn rm tags/{$destination_tag}/{$file}";
+				$output->writeln( $remove_cmd );
+				shell_exec( $cd_to_repo_cmd . $remove_cmd );
+			}
+		}
 
 		$output->writeln( '' );
 
-        // Commit changes
-        $commit_cmd = "svn commit tags/{$destination_tag} -m 'Apply modifications to {$destination_tag}'";
-        $output->writeln( "Executing SVN Command:  \n" . $commit_cmd );
-        shell_exec( $cd_to_repo_cmd . $commit_cmd );
+		// Commit changes
+		$commit_cmd = "svn commit tags/{$destination_tag} -m 'Apply modifications to {$destination_tag}'";
+		$output->writeln( "Executing SVN Command:  \n" . $commit_cmd );
+		shell_exec( $cd_to_repo_cmd . $commit_cmd );
 
-        /**
-         * The check sum below doesnt work, need some more work before its ready.
-         *
-        $svn_checksum = shell_exec( "svn info --show-item=checksum {$svn_url}/tags/{$destination_tag}" );
-        $zip_checksum = sha1_file( $zip_file );
+		/**
+		 * The check sum below doesnt work, need some more work before its ready.
+		 *
+		 * $svn_checksum = shell_exec( "svn info --show-item=checksum {$svn_url}/tags/{$destination_tag}" );
+		 * $zip_checksum = sha1_file( $zip_file );
+		 *
+		 * if ($svn_checksum !== $zip_checksum) {
+		 * $output->writeln('<error>Checksum mismatch.</error>');
+		 * return self::CMD_FAILURE;
+		 * }
+		 */
 
-        if ($svn_checksum !== $zip_checksum) {
-            $output->writeln('<error>Checksum mismatch.</error>');
-            return self::CMD_FAILURE;
-        }
-         */
+		$output->writeln( '<success>Operation completed successfully.</success>' );
 
-        $output->writeln('<success>Operation completed successfully.</success>');
+		$output->writeln( 'Deleting temporary files to cleanup workspace.' );
+		shell_exec( "rm -rf {$temp_dir}" );
 
-        $output->writeln('Deleting temporary files to cleanup workspace.');
-        shell_exec( "rm -rf {$temp_dir}" );
+		return self::CMD_SUCCESS;
+	}
 
-        return self::CMD_SUCCESS;
-    }
+	/**
+	 * Compare two directories and return an array of added and deleted files.
+	 *
+	 * @param string $source      Directory A.
+	 * @param string $destination Directory B.
+	 *
+	 * @return array|null
+	 */
+	public function compare_directories( $source, $destination ) {
+		// Ensure directories end with '/'
+		$source      = rtrim( $source, '/' ) . '/';
+		$destination = rtrim( $destination, '/' ) . '/';
 
-    /**
-     * Compare two directories and return an array of added and deleted files.
-     *
-     * @param string $source  Directory A.
-     * @param string $destination  Directory B.
-     *
-     * @return array|null
-     */
-    public function compare_directories( $source, $destination ) {
-        // Ensure directories end with '/'
-        $source = rtrim( $source, '/' ) . '/';
-        $destination = rtrim( $destination, '/' ) . '/';
+		// Check if directories exist
+		if ( ! is_dir( $source ) || ! is_dir( $destination ) ) {
+			return null;
+		}
 
-        // Check if directories exist
-        if ( ! is_dir( $source ) || ! is_dir( $destination ) ) {
-            return null;
-        }
+		// Execute rsync command to check for added files.
+		$command = "rsync --dry-run --itemize-changes --recursive --ignore-existing {$destination}/ {$source}/ | grep '>f+' | awk '{print \$2}'";
+		exec( $command, $added_in_source );
 
-        // Execute rsync command to check for added files.
-        $command = "rsync --dry-run --itemize-changes --recursive --ignore-existing {$destination}/ {$source}/ | grep '>f+' | awk '{print \$2}'";
-        exec( $command, $added_in_source );
+		// Execute rsync command to check for deleted files.
+		$command = "rsync --dry-run --itemize-changes --recursive --ignore-existing {$source}/ {$destination}/ | grep '>f+' | awk '{print \$2}'";
+		exec( $command, $removed_from_source );
 
-        // Execute rsync command to check for deleted files.
-        $command = "rsync --dry-run --itemize-changes --recursive --ignore-existing {$source}/ {$destination}/ | grep '>f+' | awk '{print \$2}'";
-        exec( $command, $removed_from_source );
-
-        return [
-            'deleted' => array_values( $removed_from_source ),  // Re-index the array
-            'added'   => array_values( $added_in_source ),    // Re-index the array
-        ];
-    }
+		return [
+			'deleted' => array_values( $removed_from_source ),  // Re-index the array
+			'added' => array_values( $added_in_source ),    // Re-index the array
+		];
+	}
 }
